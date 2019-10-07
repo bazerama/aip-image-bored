bd882cfef9cc9ed88ff83edb9ec3902eaa30182d
DESKTOP-S9ONVFR\otoon <otoon@tpg.com.au>
2019-10-07 19:55:12 +1100

    fix(post, reaction & thread models, post & thread services): last commit didn't contain file content

---
+++ src/services/post.service.js
@@ -0,0 +1,28 @@
+const Thread = require('./../api/thread');
+const Post = require('./../api/post');
+const Reaction = require('./../api/reaction');
+
+let postService = {
+    reactToPost: (reaction_id, post_id) => {
+        let post = Post.find({_id: post_id});
+        let reaction = Reaction.find({_id: reaction_id});
+
+        post.reactions.push(reaction);
+        post.save();
+    },
+    removeReaction: (reaction_id, post_id) => {
+        let post = Post.find({_id: post_id});
+        let reaction = Reaction.find({_id: reaction_id});
+
+        post.reactions.deleteOne({_id: reaction_id});
+    },
+    getReactionCount: (reaction_id, post_id) => {
+        let post = Post.find({_id: post_id});
+
+        return post.reactions.countDocuments({_id: reaction_id}, (err, c) => {
+            return c;
+        })
+    }
+};
+
+module.exports = postService;
