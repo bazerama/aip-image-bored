9d28a011ca511622946af605abdf23c867ad3863
Ruby Rhind <12567160@student.uts.edu.au>
2019-10-07 20:07:00 +1100

    feat: register merge conflict

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -103,7 +103,6 @@ class RegisterPage extends React.Component {
             passwordError: passwordPrint,
             usernameValid: usernameValid
         }, this.validateForm);
-        );
     }
 
     render() {
