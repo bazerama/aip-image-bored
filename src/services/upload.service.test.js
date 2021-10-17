3b880790a63823ef0c1692b88cea1dcb3019ee55
Horsicode <horsicode@gmail.com>
2021-10-17 06:05:17 +0000

    add test

---
+++ src/services/upload.service.test.js
@@ -0,0 +1,54 @@
+const upload_service = require("./upload.service")
+// @ponicode
+describe("upload_service.uploadImage", () => {
+    test("0", async () => {
+        await upload_service.uploadImage("libclang.dll", "user-name")
+    })
+
+    test("1", async () => {
+        await upload_service.uploadImage("InfoPlist.strings", "user-name")
+    })
+
+    test("2", async () => {
+        await upload_service.uploadImage("/results.html", "username")
+    })
+
+    test("3", async () => {
+        await upload_service.uploadImage("libclang.dylib", 123)
+    })
+
+    test("4", async () => {
+        await upload_service.uploadImage(":", "user name")
+    })
+
+    test("5", async () => {
+        await upload_service.uploadImage(undefined, undefined)
+    })
+})
+
+// @ponicode
+describe("upload_service.uploadReply", () => {
+    test("0", async () => {
+        await upload_service.uploadReply("foo bar", "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22undefined%22%20height%3D%22undefined%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%3Ctext%20x%3D%22NaN%22%20y%3D%22NaN%22%20font-size%3D%2220%22%20alignment-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20fill%3D%22white%22%3Eundefinedxundefined%3C%2Ftext%3E%3C%2Fsvg%3E", "libclang.dll", "user123")
+    })
+
+    test("1", async () => {
+        await upload_service.uploadReply("Foo bar", "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22undefined%22%20height%3D%22undefined%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%3Ctext%20x%3D%22NaN%22%20y%3D%22NaN%22%20font-size%3D%2220%22%20alignment-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20fill%3D%22white%22%3Eundefinedxundefined%3C%2Ftext%3E%3C%2Fsvg%3E", "InfoPlist.strings", 123)
+    })
+
+    test("2", async () => {
+        await upload_service.uploadReply("Hello, world!", "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22undefined%22%20height%3D%22undefined%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%3Ctext%20x%3D%22NaN%22%20y%3D%22NaN%22%20font-size%3D%2220%22%20alignment-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20fill%3D%22white%22%3Eundefinedxundefined%3C%2Ftext%3E%3C%2Fsvg%3E", "decoder.hh", "user123")
+    })
+
+    test("3", async () => {
+        await upload_service.uploadReply("This is a Text", "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22undefined%22%20height%3D%22undefined%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%3Ctext%20x%3D%22NaN%22%20y%3D%22NaN%22%20font-size%3D%2220%22%20alignment-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20fill%3D%22white%22%3Eundefinedxundefined%3C%2Ftext%3E%3C%2Fsvg%3E", "decoder.cc", "user-name")
+    })
+
+    test("4", async () => {
+        await upload_service.uploadReply("foo bar", "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22undefined%22%20height%3D%22undefined%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%3Ctext%20x%3D%22NaN%22%20y%3D%22NaN%22%20font-size%3D%2220%22%20alignment-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20fill%3D%22white%22%3Eundefinedxundefined%3C%2Ftext%3E%3C%2Fsvg%3E", "navix377.py", "user-name")
+    })
+
+    test("5", async () => {
+        await upload_service.uploadReply(undefined, undefined, undefined, "")
+    })
+})
